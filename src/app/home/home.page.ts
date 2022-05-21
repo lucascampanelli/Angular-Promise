a694b0dcabe52bf8c7dd527dba6767f66de5bb43
Lucas Campanelli <lucas.campanelli@outlook.com>
2022-05-21 08:55:00 -0300

    Subindo projeto para repositório

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,23 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public alertController: AlertController;
+  
+  constructor(alert: AlertController) {
+    this.alertController = alert;
+  }
+
+  // Método assíncrono que retorna uma promisse do tipo void (não retorna nada) que cria e exibe um alerta quando o botão for clicado
+  async presentAlert(): Promise<void> {
+    // Cria o alerta e define suas propriedades.
+    const alerta = await this.alertController.create({
+      header: 'Teste de alerta criado com promise!',
+      message: 'Alerta exibido com sucesso... E olha que eu nem esperei tanto assim. :)',
+      buttons: ['fechar']
+    });
+
+    // Exibe o alerta quando o método principal for chamado
+    alerta.present();
+  }
 
 }
